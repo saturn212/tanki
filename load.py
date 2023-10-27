54fdd07b012ba808868a2e28f44f2bd92e0b6b2f
foobar <foobar@example.com>
2023-10-27 19:30:15 +0300

    animation

--- load.py
+++ load.py
@@ -1,5 +1,6 @@
 import pygame as pg
-player_image = pg.image.load('image/player/player.png').convert_alpha()
+player_image = [pg.image.load('image/player/player 1.png').convert_alpha(),
+pg.image.load('image/player/player 2.png').convert_alpha()]
 player_bullet = pg.image.load('image/player/player bullet.png').convert_alpha()
 enemy_image = pg.image.load('image/enemy/enemy.png').convert_alpha()
 enemy_bullet = pg.image.load('image/enemy/enemy bullet.png').convert_alpha()
@@ -8,3 +9,10 @@ iron_image = pg.image.load('image/block/iron block.png').convert_alpha()
 bush_image = pg.image.load('image/block/bush block.png').convert_alpha()
 water_image = pg.image.load('image/block/water block.png').convert_alpha()
 flag_image = pg.image.load('image/block/flag.png').convert_alpha()
+bullet_image = [pg.image.load('image/player/boom/1.png').convert_alpha(),
+                pg.image.load('image/player/boom/2.png').convert_alpha(),
+                pg.image.load('image/player/boom/3.png').convert_alpha(),
+                pg.image.load('image/player/boom/4.png').convert_alpha()]
+shoot_sound = pg.mixer.Sound('sound/shot.mp3')
+boom_sound = pg.mixer.Sound('sound/boom.mp3')
+button_image = pg.image.load('image/button/button.png').convert_alpha()
--- main.py
+++ main.py
@@ -36,17 +36,23 @@ def lvlGAME():
     player_group.update()
     bullet_player_group.draw(screen)
     bullet_player_group.update()
+    bullet_enemy_group.draw(screen)
+    bullet_enemy_group.update()
     flag_group.draw(screen)
     flag_group.update()
     bush_group.draw(screen)
     bush_group.update()
     pg.display.update()
 
+def startMenu():
+    screen.fill((122, 122, 122))
+    pg.display.update()
+
 def DrawMaps(nameFile):
     maps = []
     source = "game lvl/" + str(nameFile)
     with open(source, "r") as file:
-        for i in range (0, 20):
+        for i in range(0, 20):
             maps.append(file.readline().replace("\n", "").split(",")[0:-1])
     print(maps)
     pos = [0, 0]
@@ -163,32 +169,53 @@ class Player(pygame.sprite.Sprite):
         self.rect.x = pos[0]
         self.rect.y = pos[1]
         self.dir = "top"
-        self.speed = 8
+        self.speed = 5
         self.timer_shot = 0
+        self.frame = 0
+        self.timer_anime = 0
+        self.anime = False
     def update(self):
         key = pg.key.get_pressed()
         if key[pg.K_w]:
-            self.image = pg.transform.rotate(player_image, 0)
+            self.image = pg.transform.rotate(player_image[self.frame], 0)
             self.rect.y -= self.speed
             self.dir = "top"
+            self.anime = True
         elif key[pg.K_a]:
-            self.image = pg.transform.rotate(player_image, 90)
+            self.image = pg.transform.rotate(player_image[self.frame], 90)
             self.rect.x -= self.speed
             self.dir = "left"
+            self.anime = True
         elif key[pg.K_s]:
-            self.image = pg.transform.rotate(player_image, 180)
+            self.image = pg.transform.rotate(player_image[self.frame], 180)
             self.rect.y += self.speed
             self.dir = "bottom"
+            self.anime = True
         elif key[pg.K_d]:
-            self.image = pg.transform.rotate(player_image, 270)
+            self.image = pg.transform.rotate(player_image[self.frame], 270)
             self.rect.x += self.speed
             self.dir = "right"
-
+            self.anime = True
+        else:
+            self.anime = False
         if key[pg.K_SPACE] and self.timer_shot / FPS > 1:
             bullet = Bulet_player(player_bullet, self.rect.center, self.dir)
             bullet_player_group.add(bullet)
             self.timer_shot = 0
         self.timer_shot += 1
+        shoot_sound.play()
+
+        if self.anime:
+
+            self.timer_anime += 1
+            if self.timer_anime /FPS > 0.1:
+                if self.frame == len(player_image) - 1:
+                    self.frame = 0
+                else:
+                    self.frame += 1
+                self.timer_anime = 0
+
+
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, image, pos):
         pg.sprite.Sprite.__init__(self)
@@ -199,7 +226,7 @@ class Enemy(pygame.sprite.Sprite):
         self.speed = 1
         self.dir = "top"
         self.timer_moove = 0
-
+        self.timer_shot = 0
     def update(self):
 
         self.timer_moove += 1
@@ -241,7 +268,58 @@ class Enemy(pygame.sprite.Sprite):
             elif self.dir == "right":
                 self.dir = "left"
 
+
+        if  self.timer_shot / FPS > random.randint(1,3):
+            bullet = Bulet_enemy(enemy_bullet, self.rect.center, self.dir)
+            bullet_enemy_group.add(bullet)
+            self.timer_shot = 0
+        self.timer_shot += 1
 class Bulet_player(pg.sprite.Sprite):
+    def __init__(self, image, pos, dir):
+        pg.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+        self.dir = dir
+        self.speed = 7
+        self.frame = 0
+        self.timer_anime = 0
+        self.anime = False
+        self.boom = True
+        if self.boom:
+            boom_sound.play()
+            self.boom = False
+    def update(self):
+        if self.dir == "top":
+            self.rect.y -= self.speed
+        elif self.dir == "bottom":
+            self.rect.y += self.speed
+        elif self.dir == "right":
+            self.rect.x += self.speed
+        elif self.dir == "left":
+            self.rect.x -= self.speed
+
+        if pg.sprite.spritecollide(self,enemy_group,True):
+            self.anime = True
+            self.speed = 0
+        if self.anime:
+            self.timer_anime += 1
+            if self.timer_anime / FPS > 0.1:
+                if self.frame == len(bullet_image) - 1:
+                    self.frame = 0
+                    self.rect.center = (-1000, 0)
+                    self.kill()
+                else:
+                    self.frame += 1
+                self.timer_anime = 0
+            self.image = bullet_image[self.frame]
+        pg.sprite.groupcollide(bullet_player_group, brick_group, True, True)
+        pg.sprite.groupcollide(bullet_player_group, iron_group, True, False)
+        pg.sprite.groupcollide(bullet_player_group, flag_group, True, True)
+        pg.sprite.groupcollide(bullet_player_group, enemy_group, True, True)
+
+class Bulet_enemy(pg.sprite.Sprite):
     def __init__(self, image, pos, dir):
         pg.sprite.Sprite.__init__(self)
         self.image = image
@@ -259,10 +337,26 @@ class Bulet_player(pg.sprite.Sprite):
             self.rect.x += self.speed
         elif self.dir == "left":
             self.rect.x -= self.speed
-        pg.sprite.groupcollide(bullet_player_group, brick_group, True, True)
-        pg.sprite.groupcollide(bullet_player_group, iron_group, True, False)
-        pg.sprite.groupcollide(bullet_player_group, flag_group, True, True)
-        pg.sprite.groupcollide(bullet_player_group, enemy_group, True, True)
+        pg.sprite.groupcollide(bullet_enemy_group, brick_group, True, True)
+        pg.sprite.groupcollide(bullet_enemy_group, iron_group, True, False)
+        pg.sprite.groupcollide(bullet_enemy_group, flag_group, True, True)
+
+
+class Button (pg.sprite.Sprite):
+    def __init__(self, image, pos, next_lvl, next):
+        pg.sprite.Sprite.__init__(self)
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = pos
+        self.nextlvl = next_lvl
+        self.text = text
+        text.render = font.render(self.text,True, (255, 255, 255))
+        screen.blit(text_render,(self.rect.x + 80, self.rect.y + 5))
+
+        click = pg.mouse.get_pos()
+        if pg.mouse.get_pressed()[0]:
+            if self.rect.left < click[0] < self.rect.right and self.rect.top < click[1] <self.rect.bottom:
+                lvl = self.nextlvl
 
 
 brick_group = pg.sprite.Group()
@@ -273,8 +367,10 @@ player_group = pg.sprite.Group()
 enemy_group = pg.sprite.Group()
 flag_group = pg.sprite.Group()
 bullet_player_group = pg.sprite.Group()
-player = Player(player_image, (200, 640))
+bullet_enemy_group = pg.sprite.Group()
+player = Player(player_image[0], (200, 640))
 player_group.add(player)
+button_group = pg.sprite.Group()
 DrawMaps('1.txt')
 while True:
     for event in pygame.event.get():
@@ -283,4 +379,6 @@ while True:
             sys.exit()
     if lvl == 'game':
         lvlGAME()
+    elif lvl == 'menu':
+        startMenu()
     clock.tick(FPS)
